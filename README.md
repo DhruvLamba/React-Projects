d458073264f48bbb22aa6ac175353b89106a713c
Dhruv Lamba <lambadhruv09@gmail.com>
2024-06-23 20:32:28 +0530

    currencyconvertor

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# React-Projects" 
+1) Counter
+2) Password Generator
+3) Background Changer
+4) Currency Convertor
---
+++ currencyconvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,12 @@
+import { useState,useEffect } from "react";
+function useCurrencyInfo(currency){
+    const [data,setData]=useState({})
+    useEffect(()=>{        
+        fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
+        .then((res)=> res.json() )
+        .then((res)=> setData(res[currency]))
+        console.log(data);
+    },[currency])
+    return data
+}
+export default useCurrencyInfo;
