b69a31a098eb0d82d822786c539b9165e5f55dcf
Dhruv Lamba <lambadhruv09@gmail.com>
2024-06-30 18:38:33 +0530

    reactrouter

--- currencyconvertor/src/hooks/useCurrencyInfo.js
+++ currencyconvertor/src/hooks/useCurrencyInfo.js
@@ -4,8 +4,7 @@ function useCurrencyInfo(currency){
     useEffect(()=>{        
         fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
         .then((res)=> res.json() )
-        .then((res)=> setData(res[currency]))
-        console.log(data);
+        .then((res)=> setData(res[currency]))        
     },[currency])
     return data
 }
